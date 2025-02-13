ee935949b4ad009c94cf3575302996c4f5987831
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-02-13 23:52:30 +0000

    Completed Lab 2- Assignment 2

---
+++ Labs/Lab2/NumberOperators.py
@@ -0,0 +1,16 @@
+print(14+3)
+print(14-3)
+print(14*3)
+
+print()
+
+print( 14 / 3)
+print(14 // 3)
+print(14 % 3)
+
+print()
+print(2 ** 2)
+
+print()
+print( 5 + 5 / 2 ** 2)
+print ( (5+5) / 2**2 )
