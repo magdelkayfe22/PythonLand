b730c12399c3a754a075bbe480f9e4a630df1b5a
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-02-20 23:54:56 +0000

    Completed Lecture3-Assignment1

---
+++ ClassWork/Lecture3/StringLiteral.py
@@ -0,0 +1,12 @@
+text1 = 'This is a Literal String using a single quote'
+text2 = "This is a Literal String using a double quote"
+
+print(text1)
+print(text2)
+
+
+text3 = 'This is a Literal String using a \'Single\' quote'
+text4 = "This is a Literak String using a \"double\" quote"
+
+print(text3)
+print(text4)
