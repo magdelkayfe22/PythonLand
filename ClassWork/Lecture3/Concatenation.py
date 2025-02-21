76a7fdeb4dc5ccdc19310f8530acf1bfb7616e67
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-02-21 00:52:11 +0000

    Completed Lecture3-Assignment4

---
+++ ClassWork/Lecture3/Concatenation.py
@@ -0,0 +1,14 @@
+word1 = 'Hello'
+word2 = 'World'
+
+print(word1 + word2)
+
+print( word1 + ' ' + word2 )
+
+message = word1 + ' ' + word2
+
+print(message)
+
+word1 += ' ' + word2
+
+print( word1 )
