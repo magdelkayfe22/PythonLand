bf9377c9fca0aa976c7056acc33e82992ab918fd
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-02-21 00:04:21 +0000

    Completed Lecture3-Assignment2

---
+++ ClassWork/Lecture3/MultiString.py
@@ -0,0 +1,12 @@
+text = '''This is a multiline string
+ using 'single quotes'\n
+ and "double quotes" in it.\n\n'''
+
+print(text)
+
+#print()
+
+text2 = """This is a multiline string using "double quotes" 
+and 'single quotes' in it."""
+
+print(text2)
