d09b58bc2b51c4b751dc37ef9b841b3d5dbd06f5
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-02-25 23:56:45 +0000

    Completed Lab 3-Assignment 4

---
+++ Labs/lab3/StripSpaces.py
@@ -0,0 +1,17 @@
+fruits = "      apples & pears      "
+print("0123456789012345678901234567890123456789")
+print(fruits)
+
+print(fruits + " are delicious.\n")
+
+print(fruits.rstrip() + " are delicious./n")
+
+print(fruits + " are delicious./n")
+
+print(fruits.lstrip() + "are delicious./n")
+
+print(fruits + " are delicious./n")
+
+print(fruits + " are delicious./n")
+
+print(fruits.strip().capitalize() + " are delicious./n")
