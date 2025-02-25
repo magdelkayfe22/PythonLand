0d63cedf6b114038427b0ed7255388c505281497
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-02-25 23:27:53 +0000

    Completed Lab 3-Assignment 1

---
+++ Labs/lab3/Quotes.py
@@ -0,0 +1,21 @@
+greet = "Hi there"
+print("Hi there")
+print(greet)
+
+print("")
+print('')
+print()
+
+address = '5th avenue'
+print( '5th avenue')
+print(address)
+print()
+
+print("This is a string with a 'quotation' in it. ")
+print('This is a string with a \'quotation\' in it.')
+print()
+
+txt = """1) This is a string surronded by triple double quotation with a "double quotation" in it. """
+print(txt)
+
+print('''2) This is a string surronded by triple single quotation with a 'single quotation' in it. ''')
