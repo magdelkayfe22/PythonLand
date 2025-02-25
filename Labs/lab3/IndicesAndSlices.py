dab59ca5b393a952a62db40d8995110797cbf2d3
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-02-25 23:47:17 +0000

    Completed Lab 3-Assignment 3

---
+++ Labs/lab3/IndicesAndSlices.py
@@ -0,0 +1,27 @@
+print("Python")
+print("Python"[0], "Python"[5], "Python"[:])
+
+
+str1 = "spam & eggs"
+print(str1[1])
+print(str1[2:7])
+print(str1[7:2])          #produce the empty string if m>=n in str1[m:n]
+print(str1[:4])
+print(str1[7:])
+print()
+
+print("Python")
+print("Python"[-1], "Python"[-4], "Python"[-5:-2])
+print()
+
+str1 = "spam & eggs"
+print(str1[-2])
+print(str1[-8:-3])
+print(str1[0:-1])
+print()
+
+str1 = "Python"
+print(str1[-10:10])
+print(str1[-10:3])
+print(str1[2:10])
+print()
