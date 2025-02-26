9b46ef607e51a68639624954934b96a5dc61276c
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-02-26 00:24:59 +0000

    Completed Lab 3-Lab Challenge 3

---
+++ Labs/lab3/MethodsAndFunctionsChallenge.py
@@ -0,0 +1,11 @@
+str1 = "Python coding is fun!"
+length = len(str1)
+print(length) 
+
+str1 = "Python coding is fun!"
+print(str1[13:16])
+
+print(str1.title())
+
+str1 = "Python coding is fun!"
+print(str1.count("o"))
