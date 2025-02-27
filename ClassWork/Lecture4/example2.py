5d9aaa5abba3cc08bf2d582d56843d58745c2611
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-02-27 23:51:09 +0000

    Completed Lecture 4- Assignment 1

---
+++ ClassWork/Lecture4/example2.py
@@ -0,0 +1,13 @@
+# conversion Function with Input Function
+
+number_grade = int(  input("Enter your grade(0-100): ") )
+
+average = float( input("Enter your average(0.0-100.0): ") )
+
+gpa = eval( input("Enter your Gpa(0 - 4): ") )
+
+print("Grade:", number_grade)
+
+print("Average:", average)
+
+print("GPA:", gpa)
