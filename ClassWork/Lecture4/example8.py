1c891423871cc6b2a586f642380f99f581033197
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-02-28 00:01:05 +0000

    Completed Lecture 4 – Assignment 3

---
+++ ClassWork/Lecture4/example8.py
@@ -0,0 +1,9 @@
+# Justification with Formatters
+
+print("0123456789012345678901234567")
+
+print( "{0:^5s}{1:<20s}{2:>3s}".format("Rank", "Player", "HR" ) )
+
+print( "{0:^5n}{1:<20s}{2:>3n}".format(1, "BArry Bonds", 762) )
+
+print( "{0:^5n}{1:<20s}{2:>3n}".format(3, "Babe Ruth", 714) )
