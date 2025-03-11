a52913ab5088c28a1da96c92ec944f88a401fd0b
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-03-11 23:01:50 +0000

    Completed Lecture 5- Assignment 4

---
+++ ClassWork/Lecture5/example6.py
@@ -0,0 +1,12 @@
+# creating a tuple with and without using the parentheses.
+t = 5,7,6,2
+t2 = (5, 7, 6, 2)
+print(t)
+print(t2)
+
+# 1. Finding the length of the tuple. 
+# 2. finding the max number of the tuple.
+# 3. Summing up all the numbers in the tuple.
+print( len(t), max(t), sum(t) )
+
+print(t[0], t[-1], t[ : 2 ])
