f5019bb14a355244f8cb3610005d7573717690e8
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-03-11 22:54:52 +0000

    Completed Lecture 5- Assignment 3

---
+++ ClassWork/Lecture5/example3.py
@@ -0,0 +1,11 @@
+#         0    1    2    3    4    5
+list1 = ['a', 'b', 'c', 'd', 'e', 'f']
+#.       -6   -5.  -4   -3   -2   -1
+
+print( list1[-5:-3] ) # or [1:3]
+
+print( list1[-4:-2] ) # or [2:4]
+
+print( list1[2 : len(list1)] )
+
+print( list1[3:2])
