27a951c8c10705778e7b1de81c8c2d575362ba64
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-03-18 22:27:54 +0000

    Completed Lecture 6- Assignment 1

---
+++ ClassWork/Lecture6/example1.py
@@ -0,0 +1,11 @@
+# access Values 
+
+#create a dictionary with two key/value pairs. comma separtes the dictionary and : connects the strings
+my_dictionary = {'color' : 'green', 'points' : 5}
+
+# assign the value 5 to points variable
+points = my_dictionary['points']
+
+# Format the output using the F-String
+print(f"You've just earned {points} points!")
+print(f"Do you like the color {my_dictionary['color']}?")
