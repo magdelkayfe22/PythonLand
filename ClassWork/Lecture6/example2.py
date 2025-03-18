47effe544e38ee78c4159eb106f2c7e5714a09fc
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-03-18 22:36:38 +0000

    Completed Lecture 6- Assignment 2

---
+++ ClassWork/Lecture6/example2.py
@@ -0,0 +1,12 @@
+# Adding New Key-Value pairs 
+
+#Create a dict with two key/pairs.
+my_dictionary = {'color' : 'green', 'points' : 5}
+
+# Print out the entire dictionary 
+print(my_dictionary)
+
+# Two Additional key pairs to the dictionary
+my_dictionary['first_name'] = "Tom"
+my_dictionary['last_name'] = "Arnold"
+print(my_dictionary)
