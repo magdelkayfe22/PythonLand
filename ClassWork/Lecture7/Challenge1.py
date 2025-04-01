788f3d3cc0d1fae65bde0e043c16c38cbb65ea71
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-04-01 23:23:23 +0000

    challenge 1

--- ClassWork/Lecture7/Challenge1.py
+++ ClassWork/Lecture7/Challenge1.py
@@ -1,4 +1,4 @@
 n = 6
 answ = "Yes"
 
-print('Is ((2 less than n) AND (n equal to 5+1)) OR (answ equal to "NO")', (2 < n) and ( n == 6) or (answ == "NO") )
+print('Is ((2 less than n) AND (n equal to 5+1)) OR (answ equal to "NO")', (2 < n) and ( n == 5 + 1) or (answ == "NO") )
