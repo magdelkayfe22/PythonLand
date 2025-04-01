3bb55beea6e97780691782554159b5a5159d25db
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-04-01 23:42:36 +0000

    challenge3

--- ClassWork/Lecture7/Challenge3.py
+++ ClassWork/Lecture7/Challenge3.py
@@ -1,4 +1,4 @@
 n = 6
 answ = "Yes"
 
-print('Is (n equal to 2) AND ((n equal to 7) OR (answ equal to "Yes"))', (n == 2) and ( n == 7) or (answ == "Yes") )
+print('Is (n equal to 2) AND ((n equal to 7) OR (answ equal to "Yes"))', (n == 2) and (( n == 7) or (answ == "Yes")) )
