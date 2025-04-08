fcc9ce6c368618b3c91a7defb23ca48d0dbb239a
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-04-08 23:51:12 +0000

    Completed Lecture 8- Assignment 3

--- ClassWork/Lecture8/example5.py
+++ ClassWork/Lecture8/example5.py
@@ -5,7 +5,7 @@ gpa = eval( input("Enter your gpa: ") )
 if gpa >= 3.9:
     honors = " summa cum laude"
 elif gpa>= 3.6:
-    honors = " cum laude"
+    honors = " magna cum laude"
 elif gpa>= 3.3:
     honors = " cum laude"
 else: 
