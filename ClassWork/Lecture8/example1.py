8dbae3e94879cc9b8790c0ce1b28187b8f248130
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-04-08 22:48:32 +0000

    Completed Lecture 8- Assignment 1

---
+++ ClassWork/Lecture8/example1.py
@@ -0,0 +1,11 @@
+# If-Else Statement (Decision Structure)
+
+num1 = eval( input("Enter the first number: " ) )
+num2 = eval( input("Enter the second number: ") )
+
+if num1 > num2:
+    largerValue = num1
+else:
+    largerValue = num2
+
+print("The larger value is:", str( largerValue ) + ".")
