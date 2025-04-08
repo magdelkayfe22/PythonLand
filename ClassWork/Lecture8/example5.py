0f642bf4657f5dea2f27fa78e07a3ffa82f1dd8f
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-04-08 23:09:55 +0000

    Completed Lecture 8- Assignment 3

---
+++ ClassWork/Lecture8/example5.py
@@ -0,0 +1,14 @@
+# If-Elif-Else Statements
+
+gpa = eval( input("Enter your gpa: ") )
+
+if gpa >= 3.9:
+    honors = " summa cum laude"
+elif gpa>= 3.6:
+    honors = " cum laude"
+elif gpa>= 3.3:
+    honors = " cum laude"
+else: 
+    honors = "."
+
+print("You graduated" + honors)
