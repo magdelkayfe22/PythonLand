38e22a4fb8da4a74b57c2bc36568f4e1d219a7ad
magdelkayfe22 <magd.elkayfe22@login.cuny.edu>
2025-04-30 14:28:41 +0000

    Completed Lecture 10- Assignment 1

---
+++ ClassWork/Lecture10/example2.py
@@ -0,0 +1,10 @@
+def triple(num):
+    num = 3 * num
+
+    return num
+
+num = 2
+
+print("Value before function invocation:", num)
+print("Value multiplied in function:", triple(num))
+print("Value after function invocation:", num)
